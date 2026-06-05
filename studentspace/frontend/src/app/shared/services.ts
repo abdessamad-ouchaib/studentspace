f5fee3d8969da0e067ce63272c4f7488ba5eac16
abdessamad-ouchaib <abedssamadetu2002@gmail.com>
2026-06-05 16:53:07 +0100

    Update services.ts

--- studentspace/frontend/src/app/shared/services.ts
+++ studentspace/frontend/src/app/shared/services.ts
@@ -17,9 +17,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable }    from 'rxjs';
 import { tap }           from 'rxjs/operators';
 import * as M            from './models';
-
 const API = 'https://studentspace-7uge.onrender.com/api';
-
 // ── Helper : headers avec JWT ─────────────────────────────────────────────────
 function authHeaders(): HttpHeaders {
   const token = localStorage.getItem('token');
