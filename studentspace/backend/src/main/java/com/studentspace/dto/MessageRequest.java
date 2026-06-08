f353b488ff7b0eb05042a48ff6b0f14307d2f7d6
abdessamad-ouchaib <abedssamadetu2002@gmail.com>
2026-06-08 14:49:29 +0100

    Delete studentspace/backend/src/main/java/com/studentspace/dto/MessageRequest.java

--- studentspace/backend/src/main/java/com/studentspace/dto/MessageRequest.java
+++
@@ -1,9 +0,0 @@
-package com.studentspace.dto;
-import lombok.*;
-import jakarta.validation.constraints.*;
-
-@Data @NoArgsConstructor @AllArgsConstructor @Builder
-public class MessageRequest {
-    @NotBlank private String contenu;
-    private Long moduleId;
-}
