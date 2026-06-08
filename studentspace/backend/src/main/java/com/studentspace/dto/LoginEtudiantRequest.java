61b5d69c42af82230aff939da7b7683801922819
abdessamad-ouchaib <abedssamadetu2002@gmail.com>
2026-06-08 14:49:20 +0100

    Delete studentspace/backend/src/main/java/com/studentspace/dto/LoginEtudiantRequest.java

--- studentspace/backend/src/main/java/com/studentspace/dto/LoginEtudiantRequest.java
+++
@@ -1,9 +0,0 @@
-package com.studentspace.dto;
-import lombok.*;
-import jakarta.validation.constraints.*;
-
-@Data @NoArgsConstructor @AllArgsConstructor
-public class LoginEtudiantRequest {
-    @NotBlank(message = "Le numéro APOGEE est obligatoire")
-    private String numeroApogee;
-}
