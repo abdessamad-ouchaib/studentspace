9278d2d83b15f6bd3ee1f84c8152c47fd2a05f47
abdessamad-ouchaib <abedssamadetu2002@gmail.com>
2026-06-08 14:48:51 +0100

    Delete studentspace/backend/src/main/java/com/studentspace/dto/InscriptionRequest.java

--- studentspace/backend/src/main/java/com/studentspace/dto/InscriptionRequest.java
+++
@@ -1,8 +0,0 @@
-package com.studentspace.dto;
-import lombok.*;
-
-@Data @NoArgsConstructor @AllArgsConstructor @Builder
-public class InscriptionRequest {
-    private Long etudiantId;
-    private Long moduleId;
-}
